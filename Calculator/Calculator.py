a88f178b806b5a7145c97d5efae3d7e44af18a58
Vincent <274448s@d44.ca>
2026-01-06 14:40:06 -0800

    GUI

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -2,7 +2,9 @@
 import tkinter as tk
 from tkinter import ttk
 
-root = tk()
+root = tk.Tk("Better Calculator")
 root.title = "Better Calculator"
 
 rootframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+
+root.mainloop()
