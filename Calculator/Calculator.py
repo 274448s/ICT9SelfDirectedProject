6613d259eb932450770d911b84a5fd870de80bb5
Vincent <274448s@d44.ca>
2026-01-07 14:57:58 -0800

    GUI

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,13 +1,28 @@
 # Modules
 import tkinter as tk
 from tkinter import ttk
+# Functions
+def guiscienific():
+    if scientific.get():
+        root.geometry("900x600")
+    else:
+        root.geometry("400x600")
+def switch():
+    scientific.set(not scientific.get)
+    guiscienific()
 
 # GUI
 root = tk.Tk()
 root.title("Better Calculator")
 rootframe = ttk.Frame(root, padding=(3, 3, 12, 12))
-root.geometry("400x600")
 root.resizable(False,False)
+togglescientific = ttk.Button(root, command=switch)
+togglescientific.pack()
+guiscienific()
+
+# Variables
+scientific = tk.BooleanVar(value=False)
+
 # Style
 style = ttk.Style(root)
 
