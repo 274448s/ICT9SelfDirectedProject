ecf59e9c306860e746c9499ec101bce46988def4
Vincent <274448s@d44.ca>
2026-01-07 14:43:58 -0800

    Window's Geometry added

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -6,7 +6,8 @@ from tkinter import ttk
 root = tk.Tk()
 root.title("Better Calculator")
 rootframe = ttk.Frame(root, padding=(3, 3, 12, 12))
-
+root.geometry("400x600")
+root.resizable(False,False)
 # Style
 style = ttk.Style(root)
 
