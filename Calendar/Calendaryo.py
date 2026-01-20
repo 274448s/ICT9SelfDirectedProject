153c72bef1c988142e0a1195eb13eab1183b5cc5
274448s <274448s@d44.ca>
2026-01-20 14:53:56 -0800

    Baby steps of calendar

--- Calendar/Calendaryo.py
+++ Calendar/Calendaryo.py
@@ -4,3 +4,12 @@ import tkinter as tk
 from tkinter import ttk
 import os
 import json
+import time
+
+script_path = os.path.dirname(__file__)
+# Event JSON
+print("Script dir: " + script_path)
+if os.path.exists(script_path + r'\CalendarEvents.json'):
+    print("file exists")
+else:
+    print("File created")
